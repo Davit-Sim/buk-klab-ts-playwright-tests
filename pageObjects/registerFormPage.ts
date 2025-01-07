68afc9014c45cac9333d982979e680674fedb422
Davit-Sim <davit.simonjan@gmail.com>
2025-01-07 23:59:36 +0100

    Register from tests finalized

--- pageObjects/registerFormPage.ts
+++ pageObjects/registerFormPage.ts
@@ -1,5 +1,5 @@
 import { expect, Locator, Page } from "@playwright/test";
-import { faker } from '@faker-js/faker'
+import { faker } from "@faker-js/faker";
 
 export class RegisterFormPage {
   readonly page: Page;
@@ -10,11 +10,32 @@ export class RegisterFormPage {
   readonly emailInput: Locator;
   readonly passwordInput: Locator;
   readonly confirmPasswordInput: Locator;
-  readonly submitButon: Locator;
+  readonly submitButton: Locator;
   readonly haveAnAccountText: Locator;
   readonly signInLink: Locator;
   readonly registrationSuccessfulText: Locator;
   readonly signInSectionText: Locator;
+  readonly allErrorMessages: Locator;
+  readonly emailAlreadyExistsMessages: Locator;
+
+  private readonly inputFields: Record<string, Locator>;
+
+  private readonly errorMessageMap: Record<string, string> = {
+    firstname: "First name cannot be empty or contain numbers",
+    lastname: "Last name cannot be empty or contain numbers",
+    email: "Please enter a valid email",
+    password:
+      "Password must have at least 8 characters, 1 uppercase letter and 1 number",
+    confirmpassword: "Passwords don't match",
+  };
+
+  private readonly allErrors: string[] = [
+    "First name cannot be empty or contain numbers",
+    "Last name cannot be empty or contain numbers",
+    "Please enter a valid email",
+    "Password must have at least 8 characters, 1 uppercase letter and 1 number",
+    "Passwords don't match",
+  ];
 
   constructor(page: Page) {
     this.page = page;
@@ -23,13 +44,29 @@ export class RegisterFormPage {
     this.firstNameInput = page.getByRole("textbox", { name: "First Name" });
     this.lastNameInput = page.getByRole("textbox", { name: "Last name" });
     this.emailInput = page.getByRole("textbox", { name: "Email" });
-    this.passwordInput = page.getByRole("textbox", { name: "Choose your password" });
-    this.confirmPasswordInput = page.getByRole("textbox", { name: "Confirm your password" });
-    this.submitButon = page.getByRole("button", { name: "register" });
+    this.passwordInput = page.getByRole("textbox", {
+      name: "Choose your password",
+    });
+    this.confirmPasswordInput = page.getByRole("textbox", {
+      name: "Confirm your password",
+    });
+    this.submitButton = page.getByRole("button", { name: "register" });
     this.haveAnAccountText = page.getByText("Already have an account?");
-    this.signInLink = page.locator("p").getByRole('link', { name: 'sign in' })
-    this.registrationSuccessfulText = page.getByText("registration successful!");
+    this.signInLink = page.locator("p").getByRole("link", { name: "sign in" });
+    this.registrationSuccessfulText = page.getByText(
+      "Registration successful! Check your email for a verification link."
+    );
     this.signInSectionText = page.locator('p[class*="signInSection"]');
+    this.allErrorMessages = page.locator('p[class*="errorText"]');
+    this.emailAlreadyExistsMessages = page.getByText("Email already exists", {exact: true})
+
+    this.inputFields = {
+      firstname: this.firstNameInput,
+      lastname: this.lastNameInput,
+      email: this.emailInput,
+      password: this.passwordInput,
+      confirmpassword: this.confirmPasswordInput,
+    };
   }
 
   async mainRegisterFormElementsAreVisible() {
@@ -40,89 +77,138 @@ export class RegisterFormPage {
     await expect(this.emailInput).toBeVisible();
     await expect(this.passwordInput).toBeVisible();
     await expect(this.confirmPasswordInput).toBeVisible();
-    await expect(this.submitButon).toBeVisible();
+    await expect(this.submitButton).toBeVisible();
     await expect(this.haveAnAccountText).toBeVisible();
     await expect(this.signInLink).toBeVisible();
   }
 
   async isIllustrationVisible(isDisplayed: boolean) {
-    if (isDisplayed) {
-      await expect(this.illustration).toBeVisible();
-    } else {
-      await expect(this.illustration).not.toBeVisible();
-    }
+    isDisplayed
+      ? await expect(this.illustration).toBeVisible()
+      : await expect(this.illustration).not.toBeVisible();
   }
 
-  async isSuccessfulResgitrationTextAndSignInSectionVisible(isDisplayed: boolean) {
-    if (isDisplayed) {
-      await expect(this.registrationSuccessfulText).toBeVisible();
-      await expect(this.signInSectionText).toBeVisible();
-    } else {
-      await expect(this.registrationSuccessfulText).not.toBeVisible();
-      await expect(this.signInSectionText).not.toBeVisible();
-    }
+  async isSuccessfulResgitrationTextVisible(
+    isDisplayed: boolean
+  ) {
+    isDisplayed
+      ? await expect(this.registrationSuccessfulText).toBeVisible()
+      : await expect(this.registrationSuccessfulText).not.toBeVisible();
   }
 
-  async fillOutNewValidUser(firstName?: string, lastName?: string, email?: string, password?: string, confirmPassword?: string,) {
+  async isEmailAlreadyExistsMessageVisible(
+    isDisplayed: boolean
+  ) {
+    isDisplayed
+      ? await expect(this.emailAlreadyExistsMessages).toBeVisible()
+      : await expect(this.emailAlreadyExistsMessages).not.toBeVisible();
+  }
+
+  async fillRegistrationForm(
+    firstName?: string,
+    lastName?: string,
+    email?: string,
+    password?: string,
+    confirmPassword?: string
+  ) {
     const generatedFirstName = faker.person.firstName();
     const generatedLastName = faker.person.lastName();
-    const generatedEmail = `${generatedFirstName}${generatedLastName}${faker.number.int(100)}@na.na`;
+    const generatedEmail = `${generatedFirstName}${generatedLastName}${faker.number.int(
+      1000
+    )}@pofmagic.com`;
     const genericPassword = "Password01!";
     const confirmGenericPassword = "Password01!";
 
-    firstName != undefined ? await this.firstNameInput.fill(firstName) : await this.firstNameInput.fill(generatedFirstName);
-    lastName != undefined ? await this.lastNameInput.fill(lastName) : await this.lastNameInput.fill(generatedLastName);
-    email != undefined ? await this.emailInput.fill(email) : await this.emailInput.fill(generatedEmail);
-    password != undefined ? await this.passwordInput.fill(password) : await this.passwordInput.fill(genericPassword);
-    confirmPassword != undefined ? await this.confirmPasswordInput.fill(confirmPassword) : await this.confirmPasswordInput.fill(confirmGenericPassword);
+    firstName != undefined
+      ? await this.firstNameInput.fill(firstName)
+      : await this.firstNameInput.fill(generatedFirstName);
+    lastName != undefined
+      ? await this.lastNameInput.fill(lastName)
+      : await this.lastNameInput.fill(generatedLastName);
+    email != undefined
+      ? await this.emailInput.fill(email)
+      : await this.emailInput.fill(generatedEmail);
+    password != undefined
+      ? await this.passwordInput.fill(password)
+      : await this.passwordInput.fill(genericPassword);
+    confirmPassword != undefined
+      ? await this.confirmPasswordInput.fill(confirmPassword)
+      : await this.confirmPasswordInput.fill(confirmGenericPassword);
 
-    //Validation of input field values  
-    firstName != undefined ? await expect(this.firstNameInput).toHaveValue(firstName) : await expect(this.firstNameInput).toHaveValue(generatedFirstName)
-    lastName != undefined ? await expect(this.lastNameInput).toHaveValue(lastName) : await expect(this.lastNameInput).toHaveValue(generatedLastName);
-    email != undefined ? await expect(this.emailInput).toHaveValue(email) : await expect(this.emailInput).toHaveValue(generatedEmail);
-    password != undefined ? await expect(this.passwordInput).toHaveValue(password) : await expect(this.passwordInput).toHaveValue(genericPassword);
-    confirmPassword != undefined ? await expect(this.confirmPasswordInput).toHaveValue(confirmPassword) : await expect(this.confirmPasswordInput).toHaveValue(confirmGenericPassword);
+    //Validation of input field values
+    firstName != undefined
+      ? await expect(this.firstNameInput).toHaveValue(firstName)
+      : await expect(this.firstNameInput).toHaveValue(generatedFirstName);
+    lastName != undefined
+      ? await expect(this.lastNameInput).toHaveValue(lastName)
+      : await expect(this.lastNameInput).toHaveValue(generatedLastName);
+    email != undefined
+      ? await expect(this.emailInput).toHaveValue(email)
+      : await expect(this.emailInput).toHaveValue(generatedEmail);
+    password != undefined
+      ? await expect(this.passwordInput).toHaveValue(password)
+      : await expect(this.passwordInput).toHaveValue(genericPassword);
+    confirmPassword != undefined
+      ? await expect(this.confirmPasswordInput).toHaveValue(confirmPassword)
+      : await expect(this.confirmPasswordInput).toHaveValue(
+          confirmGenericPassword
+        );
   }
 
-  async submitRegisterFormByClickingOnRegisterButton(){
-    await this.submitButon.click()
+  async submitRegisterFormByClickingOnRegisterButton() {
+    await this.submitButton.click();
   }
 
-  async iClearFollowingTextBox(value: string) {
-    const boxParsed = value.replace(/\s+/g, '').toLowerCase();
-    switch (value) {
-      case 'firstname': {
-        await this.firstNameInput.clear()
-        break;
+  async clearFields(fields: string[]) {
+    if (fields.includes("all")) {
+      for (const field of Object.keys(this.inputFields)) {
+        await this.inputFields[field].clear();
       }
-      case 'lastname': {
-        await this.lastNameInput.clear()
-        break;
-      }
-      case 'email': {
-        await this.emailInput.clear()
-        break;
-      }
-      case 'password': {
-        await this.passwordInput.clear()
-        break;
-      }
-      case 'confirmpassword': {
-        await this.confirmPasswordInput.clear()
-        break;
-      }
-      case 'all': {
-        const allInputBoxes = await this.page.locator("input", {})
-        const count = await allInputBoxes.count()
-        for (let i = 0; i < count; i++) {
-          const inputBox = allInputBoxes.nth(i);
-          await inputBox.clear();          
-        }
-        break;
-      }
-      default: {  
+      return;
+    }
 
-        break;
+    for (const field of fields) {
+      const lowerCaseField = field.toLowerCase();
+      if (this.inputFields[lowerCaseField]) {
+        await this.inputFields[lowerCaseField].clear();
+      }
+    }
+  }
+
+  async validateErrorMessagesDoExist(fields: string[]) {
+    const errorMessages = await this.allErrorMessages.allTextContents();
+
+    if (fields.includes("all")) {
+      for (const msg of this.allErrors) {
+        await expect(errorMessages).toContain(msg);
+      }
+      return;
+    }
+
+    for (const field of fields) {
+      const lowerField = field.toLowerCase();
+      if (this.errorMessageMap[lowerField]) {
+        await expect(errorMessages).toContain(this.errorMessageMap[lowerField]);
+      }
+    }
+  }
+
+  async validateErrorMessagesDoNotExist(fields: string[]) {
+    const errorMessages = await this.allErrorMessages.allTextContents();
+
+    if (fields.includes("all")) {
+      for (const msg of this.allErrors) {
+        await expect(errorMessages).not.toContain(msg);
+      }
+      return;
+    }
+
+    for (const field of fields) {
+      const lowerField = field.toLowerCase();
+      if (this.errorMessageMap[lowerField]) {
+        await expect(errorMessages).not.toContain(
+          this.errorMessageMap[lowerField]
+        );
       }
     }
   }
--- tests/registerFormPage.spec.ts
+++ tests/registerFormPage.spec.ts
@@ -20,15 +20,63 @@ test.describe("Verification of unique elements on the BooksPage", () => {
     await pm.navigateTo().navigateToJoinBukKlabPage();
     await pm.onJoinBukKlabPage().navigateToRegisterForm();
     await pm.onRegisterForm().mainRegisterFormElementsAreVisible();
-    await pm.onRegisterForm().isSuccessfulResgitrationTextAndSignInSectionVisible(false);
+    await pm.onRegisterForm().isSuccessfulResgitrationTextVisible(false);
   });
 
   test("Fill register form and create new user", async ({ page }) => {
     const pm = new PageManager(page);
     await pm.navigateTo().navigateToJoinBukKlabPage();
     await pm.onJoinBukKlabPage().navigateToRegisterForm();
-    await pm.onRegisterForm().fillOutNewValidUser();
-    //await pm.onRegisterForm().submitRegisterFormByClickingOnRegisterButton();
-    await pm.onRegisterForm().isSuccessfulResgitrationTextAndSignInSectionVisible(true);
+    await pm.onRegisterForm().fillRegistrationForm();
+    await pm.onRegisterForm().validateErrorMessagesDoNotExist(["all"]);
+    await pm.onRegisterForm().isEmailAlreadyExistsMessageVisible(false);
+    await pm.onRegisterForm().submitRegisterFormByClickingOnRegisterButton();
+    await pm.onRegisterForm().isSuccessfulResgitrationTextVisible(true);
+    await pm.onRegisterForm().isEmailAlreadyExistsMessageVisible(false);
+  });
+
+  test("Submit invalid user", async ({ page }) => {
+    const pm = new PageManager(page);
+    await pm.navigateTo().navigateToJoinBukKlabPage();
+    await pm.onJoinBukKlabPage().navigateToRegisterForm();
+    await pm.onRegisterForm().fillRegistrationForm();
+    await pm.onRegisterForm().clearFields(["firstname"]);
+    await pm.onRegisterForm().submitRegisterFormByClickingOnRegisterButton();
+    await pm.onRegisterForm().validateErrorMessagesDoExist(["firstname"]);
+    await pm.onRegisterForm().fillRegistrationForm();
+    await pm.onRegisterForm().clearFields(["lastname"]);
+    await pm.onRegisterForm().submitRegisterFormByClickingOnRegisterButton();
+    await pm.onRegisterForm().validateErrorMessagesDoExist(["lastname"]);
+    await pm.onRegisterForm().fillRegistrationForm();
+    await pm.onRegisterForm().clearFields(["email"]);
+    await pm.onRegisterForm().submitRegisterFormByClickingOnRegisterButton();
+    await pm.onRegisterForm().validateErrorMessagesDoExist(["email"]);
+    await pm.onRegisterForm().fillRegistrationForm();
+    await pm.onRegisterForm().clearFields(["password"]);
+    await pm.onRegisterForm().submitRegisterFormByClickingOnRegisterButton();
+    await pm.onRegisterForm().validateErrorMessagesDoExist(["password"]);
+    await pm.onRegisterForm().fillRegistrationForm();
+    await pm.onRegisterForm().clearFields(["confirmPassword"]);
+    await pm.onRegisterForm().submitRegisterFormByClickingOnRegisterButton();
+    await pm.onRegisterForm().validateErrorMessagesDoExist(["confirmPassword"]);
+    await pm.onRegisterForm().fillRegistrationForm("Random","User","eliska.voova@gmail.com","Password01!","Password01!");
+    await pm.onRegisterForm().submitRegisterFormByClickingOnRegisterButton();
+    await pm.onRegisterForm().isEmailAlreadyExistsMessageVisible(true);    
+  });
+
+  test("Error messagges logic verification", async ({ page }) => {
+    const pm = new PageManager(page);
+    await pm.navigateTo().navigateToJoinBukKlabPage();
+    await pm.onJoinBukKlabPage().navigateToRegisterForm();
+    await pm.onRegisterForm().fillRegistrationForm();
+    await pm.onRegisterForm().clearFields(["all"]);
+    await pm.onRegisterForm().validateErrorMessagesDoExist(["firstname", "lastname", "email", "password"]);
+    await pm.onRegisterForm().validateErrorMessagesDoNotExist(["confirmPassword"]);
+    await pm.onRegisterForm().fillRegistrationForm("First","Last","TestLast","Password","Password");
+    await pm.onRegisterForm().validateErrorMessagesDoNotExist(["firstname", "lastname", "confirmPassword"]);
+    await pm.onRegisterForm().validateErrorMessagesDoExist(["email", "password"]);
+    await pm.onRegisterForm().clearFields(["email","password", "confirmPassword"]);
+    await pm.onRegisterForm().fillRegistrationForm("","","TestLast@neco.","01","Password");
+    await pm.onRegisterForm().validateErrorMessagesDoExist(["email", "password", "confirmPassword"]);
   });
 });
